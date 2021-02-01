858ee04a0be5dc5bbdb2bb9dcf31b5ac50e7f10c
DhevanAccount01 <77318848+DhevanAccount01@users.noreply.github.com>
2021-02-01 10:36:45 +0800

    Update redirect-forget-password.php

--- redirect-forget-password.php
+++ redirect-forget-password.php
@@ -4,15 +4,34 @@ $valid = "0";
 $page = "login.php";
 
 if(isset($_POST['Submit'])){
-  if(checkPasswordSimilar($_POST['password']))
-  {
-    changePassword($_POST['email'],$_POST['password'],$_POST['repassword']);
-  }
-  else{
-    //Password already exists or not
-    echo '<script>alert("Same Password!")</script>';
-    
-  }
+    if(checkEmailExists($_POST['email']))
+    {
+        //changePassword($_POST['email'],$_POST['password'],$_POST['repassword']);
+		
+		  if(checkPasswordSimilar($_POST['password']))
+		  {
+			changePassword($_POST['email'],$_POST['password'],$_POST['repassword']);
+		  }
+		  else{
+			//Password already exists or not
+			echo '<script>alert("Same Password!")</script>';
+			
+		  }
+		changePassword($_POST['email'],$_POST['password'],$_POST['repassword']);
+		
+    }
+		
+		
+	
+    else{
+        
+		//error if email is not exist
+        echo '<script>alert("Email address is not exist")</script>';
+        //echo "<script>setTimeout(\"location.href = 'http://localhost/quizgo/html/general/forgot-password.php';\",1500);</script>";
+        echo "<script>setTimeout(\"location.href = '../../html/general/forgot-password.php';\",1500);</script>";
+    }
+  
+}
     
 
 }
